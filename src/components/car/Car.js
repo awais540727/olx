4a20fa11a0015e9028a3c7c4d8be732d2a24e334
Awais Ali <awaisali786378@gmail.com>
2025-09-03 10:57:09 +0500

    Practice before and after css selector

--- src/components/car/Car.js
+++ src/components/car/Car.js
@@ -9,6 +9,7 @@ const Car = () => {
         <button className={styles.wraper__btn1}>BUTTON 1</button>
         <button className={styles.wraper__btn2}>BUTTON 2</button>
         <button className={styles.wraper__btn3}>BUTTON 3</button>
+        <button className={styles.wraper__btn4}>BUTTON 4</button>
       </div>
     </>
   );
